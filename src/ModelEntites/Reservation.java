009b19d2af2f512d3a3f485adba913bb85528bdd
Marcos Eduardo <marcos@nouva.se>
2022-09-28 15:48:18 -0300

    Boa resolução

--- src/ModelEntites/Reservation.java
+++ src/ModelEntites/Reservation.java
@@ -1,5 +1,7 @@
 package ModelEntites;
 
+import model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -36,25 +38,25 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut) {
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException {
         Date now = new Date();
         if (checkIn.before(now) || checkOut.before(now)) {
-            return "Reservation dates for updates must be future dates.";
+            throw new DomainException("Reservation dates for updates must be future dates.");
         }
-        if (!checkOut.after(checkIn)) {
-            return "Check-out date must be after check-in date.";
-        }
+
 
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        return null;
     }
     @Override
     public String toString(){
         return "Rooom: " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut) + ", " + duration() + " nights";
     }
 
-    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException( "Check-out date must be after check-in date.");
+        }
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
